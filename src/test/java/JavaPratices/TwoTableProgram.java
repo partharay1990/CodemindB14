c0ef0887f96646d647848890a551dc00b9cdeae5
partharay1990 <parthasarathi.ray0@gmail.com>
2025-01-30 20:38:22 +0530

    adding TwoTable Program

---
+++ src/test/java/JavaPratices/TwoTableProgram.java
@@ -0,0 +1,14 @@
+package JavaPratices;
+
+public class TwoTableProgram {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10;i++) {
+			
+			System.out.print(2*i + " ");
+		}
+
+	}
+
+}
