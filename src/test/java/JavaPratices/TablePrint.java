f7db2de3fdb5b0db603bb6fb7557be182e046668
partharay1990 <parthasarathi.ray0@gmail.com>
2025-08-14 20:22:33 +0530

    added new file

--- src/test/java/JavaPratices/TablePrint.java
+++ src/test/java/JavaPratices/TablePrint.java
@@ -4,17 +4,17 @@ public class TablePrint {
 
 	public static void main(String[] args) {
 		
-		int table=30;
-		
-		for(table=1;table<=30;table++) {
-			
-			for(int i=1;i<=10;i++) {
-				
-				System.out.println(table + " * " + i + " = " + table * i);
-			}
-			
-			System.out.println("********************");
-		}
+//		int table=30;
+//		
+//		for(table=1;table<=30;table++) {
+//			
+//			for(int i=1;i<=10;i++) {
+//				
+//				System.out.println(table + " * " + i + " = " + table * i);
+//			}
+//			
+//			System.out.println("********************");
+//		}
 		
 		int num=10;
 		
